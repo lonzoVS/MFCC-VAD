2c14c4c5dedd0c077ee37421d22e28bdc6544827
LonzoVS <vadshapran@gmail.com>
2018-08-14 14:09:37 +0300

    first commit

---
+++ MFCC.cs
@@ -0,0 +1,109 @@
+﻿public class MFCC
+{
+    //Tutorial link that I used to implement MFCC below:
+    //http://practicalcryptography.com/miscellaneous/machine-learning/guide-mel-frequency-cepstral-coefficients-mfccs/
+    public double ConvertToMel(int f)
+    {
+        double mel = 1125 * Math.Log(1 + (double)f / 700);
+        return mel;
+    }
+
+    public double ConvertFromMel(double m)
+    {
+        double f = 700 * (Math.Exp(m / 1125) - 1);
+        return f;
+    }
+    public double[][] GetMFCCFilters(int mfccSize, int filterLength, int frequency, int freqMin, int freqMax)
+    {
+        double[] fb = new double[mfccSize + 2];
+        fb[0] = ConvertToMel(freqMin);
+        fb[mfccSize + 1] = ConvertToMel(freqMax);
+
+        for (int i = 1; i < mfccSize + 1; i++)
+        {
+            fb[i] = fb[0] + (double)i * (fb[mfccSize + 1] - fb[0]) / (double)(mfccSize + 1);
+        }
+        for (int i = 0; i < mfccSize + 2; i++)
+        {
+
+            fb[i] = ConvertFromMel(fb[i]);
+
+            fb[i] = Math.Floor((filterLength + 1) * fb[i] / (double)frequency);
+
+        }
+        double[][] filterBanks = new double[mfccSize][];
+        for (int m = 0; m < mfccSize; m++)
+        {
+            filterBanks[m] = new double[filterLength];
+        }
+        for (int i = 1; i < mfccSize + 1; i++)
+        {
+            for (int j = 0; j < filterLength; j++)
+            {
+
+                if (fb[i - 1] <= j && j <= fb[i])
+                {
+                    filterBanks[i - 1][j] = (j - fb[i - 1]) / (fb[i] - fb[i - 1]);
+                }
+                else if (fb[i] < j && j <= fb[i + 1])
+                {
+                    filterBanks[i - 1][j] = (fb[i + 1] - j) / (fb[i + 1] - fb[i]);
+                }
+                else
+                {
+                    filterBanks[i - 1][j] = 0;
+                }
+            }
+        }
+
+        return filterBanks;
+    }
+
+    public double[] ApplyFiltersToLogPower(double[] fftRaw, int fftSize, double[][] melFilters, int mfccCount)
+    {
+        double[] logPow = new double[mfccCount];
+
+        for (int i = 0; i < mfccCount; i++)
+        {
+            logPow[i] = 0.0;
+
+            for (int j = 0; j < fftSize; j++)
+            {
+                logPow[i] += melFilters[i][j] * Math.Pow(fftRaw[j], 2);
+            }
+
+            logPow[i] = Math.Log(logPow[i]);
+            if (double.IsInfinity(logPow[i]))
+                logPow[i] = 0;
+        }
+
+        return logPow;
+    }
+
+
+    public double[] DctTransform(double[] logData, int N)
+    {
+        double[] dct = new double[N];
+        for (int i = 0; i < N; i++)
+        {
+            dct[i] = 0;
+            for (int j = 0; j < N; j++)
+            {
+                dct[i] += logData[j] * Math.Cos(Math.PI * i * (j + 1.0 / 2.0) / N);
+            }
+        }
+
+        return dct;
+    }
+
+
+    public double[] ExecuteMFCC(double[] fftRaw, int mfccSize, int frequency, int freqMin, int freqMax, int frameLenght)
+    {
+        double[][] melFilters = GetMFCCFilters(mfccSize, frameLenght, frequency, freqMin, freqMax);
+
+        double[] logPower = ApplyFiltersToLogPower(fftRaw, fftRaw.Length, melFilters, mfccSize);
+        double[] dct = DctTransform(logPower, logPower.Length);
+
+        return dct;
+    }
+}
---
+++ MfccVadAlgorithm.cs
@@ -0,0 +1,109 @@
+﻿public class MfccVadAlgorithm 
+{
+    public double[] VadList { get; set; }
+    private double p = 0.99999999999999;
+    public List<IMask> Masks { get; set; } = new List<IMask>();
+    private int mfccSize;
+    private int freq;
+    private int freqMin;
+    private int freqMax;
+    private int frameLength;
+    private string envMaskPath;
+
+    public MfccVadAlgorithm(int mfccSize, string envMaskPath, int freq = 11025, int freqMin = 300, int freqMax = 8000, int frameLength = 256)
+    {
+        this.mfccSize = mfccSize;
+        this.freq = freq;
+        this.freqMin = freqMin;
+        this.freqMax = freqMax;
+        this.frameLength = frameLength;
+        this.envMaskPath = envMaskPath;
+    }
+
+
+    public void PerformVad(double[] samples)
+    {
+        var mfccList = PrepareMfcc(samples);
+        mfccList = AplyMasks(mfccList, envMaskPath);
+        var backGroundNoise = ComputeBackgroundNoise(mfccList.ToArray());
+        var avrgNoise = ComputeAverageNoise(backGroundNoise);
+        VadList = ComputeSimilarity(mfccList, avrgNoise);
+    }
+
+    private double[] AplyMasks(double[] mfcc, string envPath)
+    {
+        double[] mfccMasked = new double[mfcc.Length];
+        for (int i = 0; i < Masks.Count; i++)
+        {
+            mfccMasked = Masks[i].Mask(mfcc, envPath);
+        }
+        return mfccMasked;
+    }
+
+
+    //Frame = 256;
+    //windowing:  Multiplied each frames by a Hamming window;
+    //computing FFT;
+    //computing MFFC from FTT.
+    private double[] PrepareMfcc(double[] samples)
+    {
+        //DSP dsp = new DSP();
+        MFCC mfcc = new MFCC();
+        List<double> mfccList = new List<double>();
+        var spec = new RawFFTVisualization(samples).Compute(true);
+        foreach (double[] chunk in spec.Split(frameLength))
+        {
+            mfccList.AddRange(mfcc.ExecuteMFCC(chunk, mfccSize, freq, freqMin, freqMax, frameLength));
+        }
+
+        return mfccList.ToArray();
+
+    }
+
+    //Calculating the MFCC vector of all frames, assuming the first 10 frames as background noise.
+    private double[,] ComputeBackgroundNoise(double[] mfccList)
+    {
+        var noiseMFCC = mfccList.Take(10 * mfccSize).ToList();
+        List<double[]> backNoise = new List<double[]>();
+        foreach (double[] avgSlice in noiseMFCC.ToArray().Split(32))
+        {
+            backNoise.Add(avgSlice);
+        }
+        double[,] arrBUF = MathExtensions.CreateRectangularArray(backNoise);
+        return arrBUF;
+    }
+
+    //Gettin avg value of the first ten frames = initial MFCC vector of background noise.
+    private double[] ComputeAverageNoise(double[,] backNoise)
+    {
+        List<double> avrgNoise = new List<double>();
+        for (int j = 0; j < 32; j++)
+        {
+            avrgNoise.Add(MathExtensions.GetCol(backNoise, j).Average());
+        }
+        avrgNoise = avrgNoise.Select(a => a * p).ToList();
+        return avrgNoise.ToArray();
+    }
+
+    //Updating background noise vector with c_no = pc_+(1-p)c_i formula;
+    //computing similarity between c_no(background noise vector) and current mfcc vector;
+    private double[] ComputeSimilarity(double[] mfccList, double[] avrgNoise)
+    {
+        List<double> normalized = new List<double>();
+        List<double> similarity = new List<double>();
+        foreach (double[] Ci in mfccList.ToArray().Split(32))
+        {
+            var newAmounts = Ci.Select(a => a * (1.0 - p)).ToList();
+            var Cno = avrgNoise.Zip(newAmounts, (a, y) => a + y);
+            var coeffCorr = 1.0 - MathExtensions.ComputeCoeff(Ci.ToArray(), Cno.ToArray());
+            similarity.Add(coeffCorr);
+        }
+        double length = similarity.Sum(a => a * a);
+        length = Math.Sqrt(length);
+        foreach (double ddd in similarity)
+        {
+            normalized.Add(ddd / length);
+        }
+        return normalized.ToArray();
+
+    }
